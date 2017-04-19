46400503986aedb5107468877a810d2c24f4299d
galihsanjaya <galih.mpi@gmail.com>
2017-04-19 09:28:40 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Android-Studio-Simple-QRCode-Scanner
---
+++ app/src/main/java/simpleqrcodescanner/galihsanjaya/com/simpeqrcodescanner/MainActivity.java
@@ -0,0 +1,46 @@
+package simpleqrcodescanner.galihsanjaya.com.simpeqrcodescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnScan;
+    private TextView txtResult;
+
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = (Button) findViewById(R.id.btnScan);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        qrScan = new IntentIntegrator(this);
+
+        btnScan.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            txtResult.setText(result.getContents());
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    @Override
+    public void onClick(View view) {
+        qrScan.initiateScan();
+    }
+}
